0731f7c36a147602daaf7290eb95e790866022b3
Luis Flores Álvarez <luisfloresdev@gmail.com>
2019-11-08 15:16:52 -0600

    Add Horizontal layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './assets/styles/App.scss';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Horizontal from './layouts/Horizontal';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path='/login' component={Header} />
-      <Route exact path='/' component={Footer} />
+      <Route exact path='/login' component={Horizontal} />
+      <Route exact path='/' component={Horizontal} />
     </Switch>
   </BrowserRouter>
 );
