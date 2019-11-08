7a385c0c50e60ce4ad2ebb24c966a5f43fd995d0
Luis Flores Álvarez <luisfloresdev@gmail.com>
2019-11-08 14:47:53 -0600

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './assets/styles/App.scss';
+import Header from './components/Header';
 
 const App = () => (
-  <div>Hello, i am a  react app.</div>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/login' component={Header} />
+      <Route exact path='/' component={Header} />
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
