1da73d4e1e0656c39a71a55a2b6176f35ff92e78
Luis Flores Álvarez <luisfloresdev@gmail.com>
2019-11-08 14:13:33 -0600

    Add App container

---
+++ src/containers/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const App = () => (
+  <div>Hello, i am a  react app.</div>
+);
+
+export default App;
