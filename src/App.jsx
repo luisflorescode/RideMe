f55604bd64ade24429458334c6374860df80c47e
Luis Flores Álvarez <luisfloresdev@gmail.com>
2019-11-08 14:56:11 -0600

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './assets/styles/App.scss';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path='/login' component={Header} />
-      <Route exact path='/' component={Header} />
+      <Route exact path='/' component={Footer} />
     </Switch>
   </BrowserRouter>
 );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import '../assets/styles/components/Footer.scss';
+
+import logoPlatzi from '../assets/static/logo-platzi.png';
+
+const Footer = () => (
+  <footer className='footer '>
+    <div className='footer__made'>
+      <span role='img' aria-label='heart'>
+        Hecho con 💚 para la Escuela de Javascript de
+      </span>
+      <a
+        className='footer__made__link'
+        href='https://platzi.com/'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <img
+          className='footer__made__link-img'
+          src={logoPlatzi}
+          alt='logo-platzi'
+        />
+      </a>
+    </div>
+    <div className='footer__place'>
+      <span>Ciudad de México, 2019</span>
+    </div>
+  </footer>
+);
+
+export default Footer;
